eb7fede88b10d1ad144ede6a221b4f261bd1c7b4
Ege Aydın <151860320+egegeegege@users.noreply.github.com>
2026-01-08 20:23:47 +0300

    feat: JobApplicationsController has been added and authorised
    JobApplicationsController has been added and configured so that only authorised users can access it. The Index method, which lists the user's own job applications by date, and the necessary dependencies have been added. The necessary using and namespace adjustments have been made.

---
+++ ApplyFlow/Controllers/JobApplicationsController.cs
@@ -0,0 +1,35 @@
+﻿using ApplyFlow.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApplyFlow.Controllers
+{
+	[Authorize]
+	public class JobApplicationsController : Controller
+	{
+		private readonly AppDbContext _dbContext;
+		private readonly UserManager<ApplicationUser> _applicationUser;
+
+		public JobApplicationsController(
+			AppDbContext dbContext,
+			UserManager<ApplicationUser> applicationUser
+		)
+
+		{
+			
+		}
+		public async Task <IActionResult> Index()
+		{
+			var userId = _applicationUser.GetUserId(User);
+
+			var applications = await _dbContext.JobApplications
+				.Where(x => x.ApplicationUserId == userId)
+				.OrderByDescending(x => x.AppliedDate)
+				.ToListAsync();
+
+			return View(applications);
+		}
+	}
+}
