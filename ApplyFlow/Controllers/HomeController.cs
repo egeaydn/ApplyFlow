0e2791e5e14e061873c5b962f93d4efe25f03e0d
Ege Aydın <151860320+egegeegege@users.noreply.github.com>
2026-01-08 19:59:17 +0300

    feat: HomeController and (Home)index.cshtml revised

--- ApplyFlow/Controllers/HomeController.cs
+++ ApplyFlow/Controllers/HomeController.cs
@@ -6,22 +6,14 @@ namespace ApplyFlow.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
-            return View();
+            if(User.Identity != null && User.Identity.IsAuthenticated)
+            {
+               return RedirectToAction("Index", "JobApplications");
+			}
+			return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-        
     }
 }
